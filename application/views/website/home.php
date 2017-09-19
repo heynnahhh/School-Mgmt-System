bc8de77fe4d25c60f3a346e6bd3ed6e3af1add5b
AljohnPangilinan <aljohn.pangilinan@redflaggroup.com>
2017-09-19 09:42:12 +0800

    AHS Website

---
+++ application/views/website/home.php
@@ -0,0 +1,165 @@
+<?php include 'templates/header.php';?>
+
+         			<div id="featured-post-slider" class="flexslider">
+			   			<ul class="slides">
+				   			<li>
+				   				<div class="featured-post-slide">
+						   			<div class="post-background" style="background-image:url('<?php echo base_url();?>includes/website/images/thumbs/featured/featured-1.jpg');"></div>
+								   	<div class="overlay"></div>			   		
+								   	<div class="post-content">
+								   		<ul class="entry-meta">
+												<li>September 06, 2016</li> 
+												<li><a href="#" >Naruto Uzumaki</a></li>				
+											</ul>	
+
+								   		<h1 class="slide-title"><a href="single-standard.html" title="">Minimalism Never Goes Out of Style</a></h1> 
+								   	</div> 				   					  			 
+				   				</div>
+				   			</li> <!-- /slide -->
+
+				   			<li>
+				   				<div class="featured-post-slide">
+						   			<div class="post-background" style="background-image:url('<?php echo base_url();?>includes/website/images/thumbs/featured/featured-2.jpg');"></div>
+								   	<div class="overlay"></div>			   		
+								   	<div class="post-content">
+								   		<ul class="entry-meta">
+												<li>August 29, 2016</li>
+												<li><a href="#">Sasuke Uchiha</a></li>					
+											</ul>	
+
+								   		<h1 class="slide-title"><a href="single-standard.html" title="">Enhancing Your Designs with Negative Space</a></h1>
+						   			</div>		   				   					  
+			   			
+				   				</div>
+				   			</li> <!-- /slide -->
+
+				   			<li>
+				   				<div class="featured-post-slide">
+						   			<div class="post-background" style="background-image:url('<?php echo base_url();?>includes/website/images/thumbs/featured/featured-3.jpg');"></div>
+								   	<div class="overlay"></div>			   		
+								   	<div class="post-content">
+								   		<ul class="entry-meta">
+												<li>August 27, 2016</li>
+												<li><a href="#" class="author">Naruto Uzumaki</a></li>					
+											</ul>	
+
+								   		<h1 class="slide-title"><a href="single-standard.html" title="">Music Album Cover Designs for Inspiration</a></h1>
+						   			</div>
+				   				</div>
+				   			</li> <!-- end slide -->
+				   		</ul> <!-- end slides -->
+				   	</div> <!-- end featured-post-slider -->   
+					
+					
+					<div class="wrapper row3_news">
+					  <main class="hoc container clear"> 
+						  <div class="group demo">
+								<div class="group btmspace-50 demo">
+																
+								<div class="two_third first">
+														
+									<div class="headline_underline"><h6 class="news_heading"><i class="fa fa-newspaper-o" style="font-size:30px;color:maroon;"></i> Headline</h6></div>
+									
+									<h6 style="margin-top:30px;"><i>NEWS TITLEEEEEEEEEEEEE</i></h6>
+									
+									<div class = "news_container">
+									
+									<div class="date fl_left">Wednesday, September 13,2017 | </div>
+										<?php							
+											// Open the text file
+											//replace with full path from db
+											$f = fopen("includes/website/asd.txt", "r");
+											
+											echo fgets($f) . "<br />";
+													
+											// Close the text file
+											fclose($f);
+										?>
+									</div>
+								
+									  <div id="footer" style="margin-top:-40px;" class="fl_right"><a href="news"><button>See full details &raquo;</button></a></div>
+									    
+									<div class ="more_news">
+									
+									<div class="headline_underline"><h6 class="more_news_heading "><i class="fa fa-newspaper-o" style="font-size:30px;color:maroon;"></i> More News</h6></div>
+									
+									<div class="wrapper row3">
+									  <section class="hoc container_more_news clear">
+										<!-- ################################################################################################ -->
+										<ul class="nospace group">
+										  <li class="one_third first">
+											<article class="excerpt"><a href="#"><img class="inspace-10 borderedbox" src="<?php echo base_url();?>includes/website/images/demo/320x220.png" alt=""></a>
+											  <div class="excerpttxt">
+												<ul>
+												  <li><i class="fa fa-calendar-o"></i> 06/01/45</li>
+												</ul>
+												<h6 class="heading font-x1">News Title</h6>
+												<p><a class="btn" href="news">Read More &raquo;</a></p>
+											  </div>
+											</article>
+										  </li>
+										  <li class="one_third">
+											<article class="excerpt"><a href="#"><img class="inspace-10 borderedbox" src="<?php echo base_url();?>includes/website/images/demo/320x220.png" alt=""></a>
+											  <div class="excerpttxt">
+												<ul>
+												  <li><i class="fa fa-calendar-o"></i> 05/01/45</li>
+
+												</ul>
+												<h6 class="heading font-x1">News Title</h6>
+												<p><a class="btn" href="news">Read More &raquo;</a></p>
+											  </div>
+											</article>
+										  </li>
+										  <li class="one_third">
+											<article class="excerpt"><a href="#"><img class="inspace-10 borderedbox" src="<?php echo base_url();?>includes/website/images/demo/320x220.png" alt=""></a>
+											  <div class="excerpttxt">
+												<ul>
+												  <li><i class="fa fa-calendar-o"></i> 04/01/45</li>
+
+												</ul>
+												<h6 class="heading font-x1">News Title</h6>
+												<p><a class="btn" href="news">Read More &raquo;</a></p>
+											  </div>
+											</article>
+										  </li>
+										</ul>
+										<!-- ################################################################################################ -->
+									  </section>
+									</div>
+
+								</div>
+ 								
+									</div>	
+																			
+									<div class="one_third">
+									
+											<div class="headline_underline"><h6 class="news_heading"><i class="fa fa-calendar-check-o" style="font-size:30px;color:maroon;"></i> Upcoming Events</h6></div>
+											
+											<div class = "news_container">DASDASDASDAS</div>
+										
+										<div class="calendar_container">
+										
+											<div class="headline_underline"><h6 class="news_heading"><i class="fa fa-calendar" style="font-size:30px;color:maroon;"></i> School Calendar</h6></div>
+											
+											<div class = "news_container">
+										
+										DASDASDASDAS
+										DASDASDASDASDASDASDASDAS
+										DASDASDASDAS
+										
+										</div>		
+										
+									<div id="footer" style="margin:-50px -1px;" class="fl_right"><a href="#"><button>View calendar</button></a></div>
+							</div>
+												
+						  </div>
+						  					
+						</div>						
+							
+						<div class="clear"></div>
+					  </main>
+					</div>
+				</div>
+					
+					
+<?php include 'templates/footer.php';?>
---
+++ application/views/website/templates/header.php
@@ -0,0 +1,74 @@
+<html lang="">
+
+<head>
+<title>Araullo High School</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+<meta name="format-detection" content="telephone=no" />
+<link href="<?php echo base_url();?>includes/website/css/layout.css" rel="stylesheet" type="text/css" media="all">
+
+</head>
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<body id="top">
+<div class="wrapper row0" onLoad = "renderTime();">
+  <div id="topbar" class="hoc clear">
+    <!-- ################################################################################################ -->
+    <div class="fl_left">
+      <ul class="nospace">
+		<li><div id = "clockDisplay"></div></li>
+        <li><i class="fa fa-phone"></i> +63 915 933 5058</li>
+        <li><i class="fa fa-envelope-o"></i> araullo@araullo@email.com</li>
+      </ul>
+    </div>
+    <div class="fl_right">
+      <ul class="nospace">
+	    <li><a href="home">Home</a></li>
+      <li><a href="ers">ERS</a></li>
+		  <li><a href="#">AHS-LRMDS</a></li>
+      </ul>
+    </div>
+    <!-- ################################################################################################ -->
+  </div>
+</div>
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<div class="wrapper row1">
+  <header id="header" class="hoc clear">
+    <!-- ################################################################################################ -->
+    <div id="logo" class="fl_left">
+     <h1><a href="home"><img src = "<?php echo base_url();?>includes/website/images/demo/logo.png" style= "height:175px; width:170px;"><div id='title'>Araullo High School</div></a></h1>
+    </div>
+      <nav id="mainav" class="fl_right">
+      <ul class="clear">
+        <li class="has-children">
+          <a class="drop" href="#">About</a>
+          <ul class="sub-menu">
+            <li><a href="#">History</a></li>
+            <li><a href="#">Mission, Vision, Goals & Objectives</a></li>
+            <li><a href="#">Organizational Chart</a></li>
+          </ul>
+        </li>
+        <li><a class="drop" href="#">Admission</a>
+          <ul>
+            <li><a href="#">Admission Procedures</a></li>
+            <li><a href="#">Programs</a></li>
+            <li><a href="#">Courses Offered (SHS)</a></li>
+          </ul>
+        </li>
+        <li><a class="drop" href="#">Students</a>
+              <ul>
+                <li><a href="ers">ERS</a></li>
+                <li><a href="#">Academic Calendar</a></li>
+                <li><a href="#">Enrollment Procedures</a></li>
+              </ul>
+        </li>
+        <li><a href="#">AHS-LRMDS</a></li>
+        <li><a href="contacts">Contact Us</a></li>
+      </ul>
+    </nav>
+    <!-- ################################################################################################ -->
+  </header>
+</div>
