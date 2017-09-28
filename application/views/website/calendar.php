b512d2a2662b48c0ae4666ca66abef07cfa5323a
Hannah Liao <hannah_liao@redflaggroup.com>
2017-09-28 19:15:56 +0800

    Controller and view for calendar

---
+++ application/views/website/calendar.php
@@ -0,0 +1,317 @@
+<?php include 'templates/header.php';?>
+
+<div class="col-twelve">
+	<div class="wrapper row3">
+		<main class="hoc hoc_all container clear">
+			<div class="group demo">
+				<div class="group btmspace-50 demo">
+
+					<div class="two_third first">
+
+						<div class="headline_underline"><center><h6 class="news_heading">
+							<i class="fa fa-calendar" style="font-size:30px;color:maroon;"></i> School Calendar SY 2017-2018</h6></center>
+						</div>
+
+				<!-- ################# ################# #Information ################# ################# ################# -->
+							
+								 <div class="table-responsive">
+								  <table class="table table-hover">
+									<thead>
+									  <tr>
+										<th>DATE</th>
+										<th><center>ACTIVITIES</center></th>
+									  </tr>
+									</thead>
+									<tbody>
+									  	<tr>
+											<td colspan="2" style="font-style: italic;" class="bold">JUNE</td>
+									  	</tr>
+
+									  	<tr>
+											<td>June 3</td>
+											<td><center>General Assembly and 1<sup>st</sup> Parent-Teacher Conference</center></td>
+									 	 </tr>
+
+									  	<tr>
+											<td>June 5</td>
+											<td><center>Beginning of Classes</center></td>
+									 	 </tr>
+
+									  	<tr>
+											<td>June 12</td>
+											<td><center>Independence Day (Regular Holiday)</center></td>
+									  	</tr>
+
+									  	<tr>
+											<td>June 26</td>
+											<td><center>Eid'l Fitr (Regular Holiday)</center></td>
+										 </tr>
+
+										<tr>
+											<td>Tentative</td>
+											<td><center>Early Language Literacy and Numeracy Assessment (ELLNA)</center></td>
+									  	</tr>
+
+									 	<tr>
+											<td>Tentative</td>
+											<td><center>National Achievement Test (NAT) For Grade 6</center></td>
+									  	</tr>
+
+									  	<tr>
+											<td>Tentative</td>
+											<td><center>National Achievement Test (NAT) For Grade 10</center></td>
+									  	</tr>
+
+									  	<tr>
+											<td colspan="2" style="font-style: italic;" class="bold">AUGUST</td>
+									  	</tr>
+
+									  	<tr>
+											<td>August 10-11</td>
+											<td><center>First Quarter Examinations</center></td>
+									  	</tr>
+
+									  	<tr>
+											<td>August 19</td>
+											<td><center>Distribution of Report Cards and 2<sup>nd</sup> Parent-Teacher Conference</center></td>
+									  	</tr>
+
+									  	<tr>
+											<td>August 21</td>
+											<td><center>Ninoy Aquino Day (Special Non-working Holiday)</center></td>
+									  	</tr>
+
+									  	<tr>
+											<td>August 28</td>
+											<td><center>National Heroes Day (Regular Holiday)</center></td>
+									  	</tr>
+
+									  	<tr>
+											<td>Tentative</td>
+											<td><center>National Career Assessment Examination (NCAE) for Grade 9</center></td>
+									  	</tr>
+
+									  	<tr>
+											<td colspan="2" style="font-style: italic;" class="bold">SEPTEMBER</td>
+									  	</tr>
+
+									  	<tr>
+											<td>September 1</td>
+											<td><center>Eid'l Adha (Special Non-working Holiday)</center></td>
+									  	</tr>
+
+									  	<tr>
+											<td colspan="2" style="font-style: italic;" class="bold">OCTOBER</td>
+									  	</tr>
+
+									  	<tr>
+											<td>October 1</td>
+											<td><center>Alternative Learning System Accreditation and Equivalency (ALS A&E) Test</center></td>
+									  	</tr>
+
+									  	<tr>
+											<td>October 5</td>
+											<td><center>World Teachers' Day</center></td>
+									  	</tr>
+
+									  	<tr>
+											<td>October 19-20</td>
+											<td><center>Second Quarter Examinations</center></td>
+									  	</tr>
+
+									  	<tr>
+											<td style="padding-top: 15px">October 23-27</td>
+											<td><center>Start of Sembreak <br> In-Service Training for Teachers</center></td>
+									  	</tr>
+
+									  	<tr>
+											<td>October 28</td>
+											<td><center>Distribution of Report Cards and 3<sup>rd</sup> Parent-Teacher Conference</center></td>
+									  	</tr>
+
+									  	<tr>
+											<td>October 30</td>
+											<td><center>Instructional Materials (IM) Preparation</center></td>
+									  	</tr>
+
+									  	<tr>
+											<td>October 31</td>
+											<td><center>Additional Special Holiday</center></td>
+									  	</tr>
+
+									  	<tr>
+											<td colspan="2" style="font-style: italic;" class="bold">NOVEMBER</td>
+									  	</tr>
+
+									  	<tr>
+											<td>November 1</td>
+											<td><center>All Saints Day (Special Non-working Holiday)</center></td>
+									  	</tr>
+
+									  	<tr>
+											<td>November 2</td>
+											<td><center>Resumption of Classes</center></td>
+									  	</tr>
+
+									  	<tr>
+											<td>November 30</td>
+											<td><center>All Saints Day (Special Non-working Holiday)</center></td>
+									  	</tr>
+
+									  	<tr>
+											<td>TBA</td>
+											<td><center>National Achievement Test (NAT) for Grade 12</center></td>
+									  	</tr>
+
+									  	<tr>
+											<td colspan="2" style="font-style: italic;" class="bold">DECEMBER</td>
+									  	</tr>
+
+									  	<tr>
+											<td>December 22</td>
+											<td><center>Start of Christmas Break</center></td>
+									  	</tr>
+
+									  	<tr>
+											<td>December 25</td>
+											<td><center>Christmas Day (Regular Holiday)</center></td>
+									  	</tr>
+
+									  	<tr>
+											<td>December 31</td>
+											<td><center>New Year's Eve (Special Non-working Holiday)</center></td>
+									  	</tr>
+
+									  	<tr>
+											<td colspan="2" style="font-style: italic;" class="bold">JANUARY</td>
+									  	</tr>
+
+									  	<tr>
+											<td>January 3</td>
+											<td><center>Resumption of Classes</center></td>
+									  	</tr>
+
+									  	<tr>
+											<td>January 11-12</td>
+											<td><center>Third Quarter Examinations</center></td>
+									  	</tr>
+
+									  	<tr>
+											<td>January 20</td>
+											<td><center>Distribution of Report Cards and 4<sup>th</sup> Parent-Teacher Conference</center></td>
+									  	</tr>
+
+									  	<tr>
+											<td colspan="2" style="font-style: italic;" class="bold">FEBRUARY</td>
+									  	</tr>
+
+									  	<tr>
+											<td>February 16</td>
+											<td><center>Chinese Lunar New Year's Day</center></td>
+									  	</tr>
+
+									  	<tr>
+											<td rowspan="2" style="padding-top: 25px">February 19-23</td>
+											<td><center>National Festival of Talents (NFOT)</center></td>
+									  	</tr>
+
+									  	<tr>
+											<td><center>National Schools Press Conference (NSPC)</center></td>
+									  	</tr>
+
+									  	<tr>
+											<td colspan="2" style="font-style: italic;" class="bold">MARCH</td>
+									  	</tr>
+
+									  	<tr>
+											<td>March 15-16</td>
+											<td><center>Fourth Quarter Examinations for Grade 6 and 12</center></td>
+									  	</tr>
+
+									  	<tr>
+											<td>March 19-20</td>
+											<td><center>Fourth Quarter Examinations for Grades 1-5 and Grades 7-11</center></td>
+									  	</tr>
+
+									  	<tr>
+											<td>March 29</td>
+											<td><center>Maundy Thursday (Regular Holiday)</center></td>
+									  	</tr>
+
+									  	<tr>
+											<td>March 30</td>
+											<td><center>Good Friday (Regular Holiday)</center></td>
+									  	</tr>
+
+									  	<tr>
+											<td colspan="2" style="font-style: italic;" class="bold">APRIL</td>
+									  	</tr>
+
+									  	<tr>
+											<td style="padding-top: 30px">April 2-6</td>
+											<td><center>End of School Year Rites<br>Graduation of Grade 12<br>Moving up Ceremony of Grade 10</center></td>
+									  	</tr>
+
+									  	<tr>
+											<td>April 6</td>
+											<td><center>Last Day of Classes for SY 2017-2018</center></td>
+									  	</tr>
+
+									  	<tr>
+											<td>April 9</td>
+											<td><center>Araw ng Kagitingan (Regular Holiday)</center></td>
+									  	</tr>
+
+									  	<tr>
+											<td>April 10-11</td>
+											<td><center>Distribution of Report Cards and 5<sup>th</sup> Parent-Teacher Conference</center></td>
+									  	</tr>
+
+									  	<tr>
+											<td>April 16-20</td>
+											<td><center>Palarong Pambansa</center></td>
+									  	</tr>
+
+									  	<tr>
+											<td>April 16 - May 25</td>
+											<td><center>Summer Classes</center></td>
+									  	</tr>
+
+				<!-- ################################################################################################ -->
+									</tbody>
+								  </table>
+
+								 </div>
+								 
+					</div>
+
+					<div class="one_third">
+						<div class = "right_sidebar">
+							<div class="article_underline"><h6 class="sidebar_title"><i class="fa fa-calendar-check-o"></i> Upcoming Events</h6></div>
+							<div class="sidebar_container">DASDASDASDAS</div>
+						</div>
+
+						<div class="right_sidebar">
+							<div class="article_underline"><h6 class="sidebar_title"><i class="fa fa-calendar"></i> School Calendar</h6></div>
+							<div class = "sidebar_container">
+								DASDASDASDAS
+								DASDASDASDASDASDASDASDAS
+								DASDASDASDAS
+							</div>
+							<div id="footer_button" style="margin:-50px -1px;" class="fl_right"><a class="btn" href="<?php echo site_url('website/calendar')?>">View calendar</a></div>
+						</div>
+
+					</div>
+
+				</div>
+
+				<div class="clear"></div>
+
+		  </main>
+
+		</div>
+
+	</div>
+</div>
+
+<?php include 'templates/footer.php';?>
