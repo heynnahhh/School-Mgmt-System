886f8bb8ecc149943e4e0aa9562d15a7c6dfb4b0
niggspat <johnpatrickguzman123@gmail.com>
2017-09-30 16:43:47 +0800

    Some changes in history codes

--- application/views/website/history.php
+++ application/views/website/history.php
@@ -12,69 +12,69 @@
 							<h6 class="news_heading">History</h6>
 								</div>					
 									<div class = "history">
-										<h6>
-											<div class="img-container">
-												<img src = "<?php echo base_url('includes/website/images/a.jpg');?>">
-													<p><i>Manuel Gonzales Araullo</br>(January 1, 1853 – July 26, 1924) </i></p>
-												</div>
-												It was called the Manila High School when it was opened in 1906 up 
-												to 1921, when it got its new name, Manila South High School, which 
-												it used until 1930, when it was finally given its present name in 
-												honor of, by then, the late Chief Justice of the Philippines Manuel 
-												Araullo. <br><br>
+										<div class="img-container">
+											<img src = "<?php echo base_url('includes/website/images/a.jpg');?>">
+												<h6><i>Manuel Gonzales Araullo</br>(January 1, 1853 – July 26, 1924) </i></h6>
+											</div>
+												<p>
+													It was called the Manila High School when it was opened in 1906 up 
+													to 1921, when it got its new name, Manila South High School, which 
+													it used until 1930, when it was finally given its present name in 
+													honor of, by then, the late Chief Justice of the Philippines Manuel 
+													Araullo. <br><br>
 
-												Its brethren schools in the City of Manila—Arellano High, Torres 
-												High and Abad Santos High—were all named after Supreme Court 
-												justices. Arellano High is named to honor the first Chief Justice of 
-												the Philippines, Cayetano Arellano. Before that, Arellano High was 
-												called the Manila North High School throughout the American and 
-												Commonwealth periods of our history. <br><br>
+													Its brethren schools in the City of Manila—Arellano High, Torres 
+													High and Abad Santos High—were all named after Supreme Court 
+													justices. Arellano High is named to honor the first Chief Justice of 
+													the Philippines, Cayetano Arellano. Before that, Arellano High was 
+													called the Manila North High School throughout the American and 
+													Commonwealth periods of our history. <br><br>
 
-												Torres High is named for Associate Justice Florentino Torres. Abad 
-												Santos High honors Chief Justice Abad Santos, who was not only a 
-												model in upholding the rule of law, civil liberties and commitment 
-												to dispense justice fairly but died for his commitment as a martyr. 
-												He was executed by the Japanese invaders. <br><br>
+													Torres High is named for Associate Justice Florentino Torres. Abad 
+													Santos High honors Chief Justice Abad Santos, who was not only a 
+													model in upholding the rule of law, civil liberties and commitment 
+													to dispense justice fairly but died for his commitment as a martyr. 
+													He was executed by the Japanese invaders. <br><br>
 
-												Although the Araullo, Arellano, Torres and Abad Santos high schools 
-												also excel in their own ways these days, there is something lacking 
-												about them now that somehow places them below the level of certain 
-												iconic private schools, some specialized Science high schools and 
-												the UP High School.<br><br>
+													Although the Araullo, Arellano, Torres and Abad Santos high schools 
+													also excel in their own ways these days, there is something lacking 
+													about them now that somehow places them below the level of certain 
+													iconic private schools, some specialized Science high schools and 
+													the UP High School.<br><br>
 
-												During the Commonwealth period and up to the 1950s, the public 
-												schools under the Division of City Schools of the City of Manila 
-												were the producers of some of the best minds to emerge into the 
-												national consciousness as educators, writers, economists, diplomats, 
-												lawyers and even presidents. In those days the graduates of these 
-												schools—in the main—could all be relied on to pass on the basic 
-												virtues and a sense of honor to their children. In the days before 
-												World War II it was said that the graduates of these schools could 
-												teach basic education—in good English—to their own children and to 
-												formal pupils if they were hired to teach right away.<br><br>
+													During the Commonwealth period and up to the 1950s, the public 
+													schools under the Division of City Schools of the City of Manila 
+													were the producers of some of the best minds to emerge into the 
+													national consciousness as educators, writers, economists, diplomats, 
+													lawyers and even presidents. In those days the graduates of these 
+													schools—in the main—could all be relied on to pass on the basic 
+													virtues and a sense of honor to their children. In the days before 
+													World War II it was said that the graduates of these schools could 
+													teach basic education—in good English—to their own children and to 
+													formal pupils if they were hired to teach right away.<br><br>
 
-												Those who are still around after the tutelage they got from their 
-												Araullo, Arellano, Torres and Abad Santos teachers in the fifties 
-												and earlier will swear how their English, Math, Biology, General 
-												Science and Vocational Education teachers all contributed to their 
-												skills and their knowledge, to their ability to comprehend and 
-												absorb higher studies at colleges and universities.<br><br>
+													Those who are still around after the tutelage they got from their 
+													Araullo, Arellano, Torres and Abad Santos teachers in the fifties 
+													and earlier will swear how their English, Math, Biology, General 
+													Science and Vocational Education teachers all contributed to their 
+													skills and their knowledge, to their ability to comprehend and 
+													absorb higher studies at colleges and universities.<br><br>
 
-												Among the alumni were three former presidents, 
-												Manuel A. Roxas, Jose P. Laurel and Elpidio Quirino; the inimitable 
-												foreign secretary and first president of the United Nations General 
-												Assembly Carlos P. Romulo; and the country's grand dame of 
-												pediatricians, Dr. Fe del Mundo.<br><br>
+													Among the alumni were three former presidents, 
+													Manuel A. Roxas, Jose P. Laurel and Elpidio Quirino; the inimitable 
+													foreign secretary and first president of the United Nations General 
+													Assembly Carlos P. Romulo; and the country's grand dame of 
+													pediatricians, Dr. Fe del Mundo.<br><br>
 
 
-												How much happier and fulfilled the present generation of Filipinos 
-												would now be if Araullo High and its brethren, and the many 
-												government high schools that were opened after the Second World War, 
-												had maintained the standards of teaching and discipline that were 
-												brought to this archipelago by the Thomasites.<br><br>
-										</h6>
+													How much happier and fulfilled the present generation of Filipinos 
+													would now be if Araullo High and its brethren, and the many 
+													government high schools that were opened after the Second World War, 
+													had maintained the standards of teaching and discipline that were 
+													brought to this archipelago by the Thomasites.<br><br>
+												</p>
 									</div>			
-							</div>
+					</div>
 
 					<div class="one_third">
 						<div class = "right_sidebar">
