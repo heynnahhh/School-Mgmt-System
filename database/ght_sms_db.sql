275853ab49065a7f4eb4c32d539c7a7880cae841
AljohnPangilinan <aljohn.pangilinan@redflaggroup.com>
2017-10-11 10:34:57 +0800

    database

--- database/ght_sms_db.sql
+++ database/ght_sms_db.sql
@@ -1,13 +1,15 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.4
+-- version 4.7.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 08, 2017 at 05:26 PM
--- Server version: 5.7.14
--- PHP Version: 5.6.25
+-- Generation Time: Oct 11, 2017 at 04:34 AM
+-- Server version: 10.1.22-MariaDB
+-- PHP Version: 7.1.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -46,7 +48,8 @@ INSERT INTO `addresses` (`id`, `students_info_lrn`, `street`, `barangay`, `city`
 (4, 44444, 'Golden st.', 'Barangay Golden', 'Dasma', 'Cavite'),
 (5, 55555, 'Citta st,', 'Barangay Italia', 'Imus', 'Cavite'),
 (6, 66666, 'LA street', 'Baranggay LA', 'LA city', 'LA province'),
-(7, 14, '226-b', 'Talaba I', 'Bacoor City', 'Cavity');
+(7, 14, '226-b', 'Talaba I', 'Bacoor City', 'Cavity'),
+(8, 0, 'qweqweqweqw', 'eqweqweqw', 'eqweqw', 'eqweqwe');
 
 -- --------------------------------------------------------
 
@@ -73,7 +76,8 @@ INSERT INTO `guardians` (`id`, `students_info_lrn`, `name`, `contact`, `relation
 (4, 44444, 'Mother Salva', 'MotherSalva con', 'Mother'),
 (5, 55555, 'Roman Ferrer', 'FatherFerrer co', 'Father'),
 (6, 66666, 'Father Urie', '092222', 'Father'),
-(7, 14, 'Marissa Tomei', '0955-887-4822', 'Mother');
+(7, 14, 'Marissa Tomei', '0955-887-4822', 'Mother'),
+(8, 0, 'qweqw', '12312312', 'qweqweqwqw');
 
 -- --------------------------------------------------------
 
@@ -101,7 +105,8 @@ INSERT INTO `parents` (`id`, `students_info_lrn`, `mother_name`, `mother_contact
 (4, 44444, 'Mother Salva', 'MotherSalva con', 'Father Salva', 'FatherSalva con'),
 (5, 55555, 'fukIforgot Ferrer', 'MotherFerrer co', 'Roman Ferrer', 'FatherFerrer co'),
 (6, 66666, 'Mother Urie', '0944444', 'Father Urie', '092222'),
-(7, 14, 'Marissa Tomei', '0955-887-4822', 'Tom Cruise', '0955-887-4822');
+(7, 14, 'Marissa Tomei', '0955-887-4822', 'Tom Cruise', '0955-887-4822'),
+(8, 0, 'eqweqw', '21312321', 'eqweqwe', '21312321');
 
 -- --------------------------------------------------------
 
@@ -132,13 +137,14 @@ CREATE TABLE `students_info` (
 --
 
 INSERT INTO `students_info` (`id`, `lrn`, `first_name`, `middle_name`, `last_name`, `sex`, `contact_number`, `birth_date`, `birth_place`, `age`, `mother_tongue`, `religion`, `online_applicant`, `date_registered`, `date_modified`) VALUES
-(1, 11111, 'Adrielle Kristine Nicolette', 'Mestiola', 'Escaro', 'Female', '09355156653', '1998-06-24', 'Quezon City', 19, 'Tagalog', 'Catholic', b'0', '2017-09-19 17:17:57', '2017-09-19 17:17:57'),
-(2, 22222, 'Hakeem', 'Joshua', 'Polistico', 'Male', '09558874822', '1995-07-22', 'Hospital Bed', 22, 'Tagalog', 'Catholic', b'0', '2017-09-19 17:29:40', '2017-09-19 17:29:40'),
-(3, 33333, 'John', 'Patrick', 'Guzman', 'Male', '09333333333', '2017-09-05', 'Hospital Bed', 0, 'Tagalog', 'Catholic', b'0', '2017-09-19 17:32:58', '2017-09-19 17:32:58'),
-(4, 44444, 'Jasver', 'Anlouis', 'Salva', 'Female', '09444444444', '2017-09-22', 'Hospital Bed', 0, 'Tagalog', 'Catholic', b'0', '2017-09-19 17:35:08', '2017-09-19 17:35:08'),
-(5, 55555, 'Journacel', 'Urgel', 'Ferrer', 'Female', '09555555555', '1997-10-10', 'Hospital Bed', 19, 'Tagalog', 'Catholic', b'0', '2017-09-19 17:37:59', '2017-09-19 17:37:59'),
-(6, 66666, 'Brendon', 'Pogi', 'Urie', 'Male', '09666666666', '1998-07-29', 'Los Angeles', 19, 'English', 'no one knows', b'0', '2017-10-01 19:16:21', '2017-10-01 19:16:21'),
-(7, 14, 'Hakeem', 'Andaya', 'Polistico', 'Male', '0955-887-4822', '1994-07-22', 'Mandaluyong City', 23, 'Filipino', 'Roman Catholic', b'0', '2017-10-01 23:03:03', '2017-10-01 23:03:03');
+(1, 11111, 'Adrielle Kristine Nicolette', 'Mestiola', 'Escaro', 'Female', '09355156653', '1998-06-24', 'Quezon City', 19, 'Tagalog', 'Catholic', b'1111111111111111111111111111111', '2017-09-19 17:17:57', '2017-09-19 17:17:57'),
+(2, 22222, 'Hakeem', 'Joshua', 'Polistico', 'Male', '09558874822', '1995-07-22', 'Hospital Bed', 22, 'Tagalog', 'Catholic', b'1111111111111111111111111111111', '2017-09-19 17:29:40', '2017-09-19 17:29:40'),
+(3, 33333, 'John', 'Patrick', 'Guzman', 'Male', '09333333333', '2017-09-05', 'Hospital Bed', 0, 'Tagalog', 'Catholic', b'1111111111111111111111111111111', '2017-09-19 17:32:58', '2017-09-19 17:32:58'),
+(4, 44444, 'Jasver', 'Anlouis', 'Salva', 'Female', '09444444444', '2017-09-22', 'Hospital Bed', 0, 'Tagalog', 'Catholic', b'1111111111111111111111111111111', '2017-09-19 17:35:08', '2017-09-19 17:35:08'),
+(5, 55555, 'Journacel', 'Urgel', 'Ferrer', 'Female', '09555555555', '1997-10-10', 'Hospital Bed', 19, 'Tagalog', 'Catholic', b'1111111111111111111111111111111', '2017-09-19 17:37:59', '2017-09-19 17:37:59'),
+(6, 66666, 'Brendon', 'Pogi', 'Urie', 'Male', '09666666666', '1998-07-29', 'Los Angeles', 19, 'English', 'no one knows', b'1111111111111111111111111111111', '2017-10-01 19:16:21', '2017-10-01 19:16:21'),
+(7, 14, 'Hakeem', 'Andaya', 'Polistico', 'Male', '0955-887-4822', '1994-07-22', 'Mandaluyong City', 23, 'Filipino', 'Roman Catholic', b'1111111111111111111111111111111', '2017-10-01 23:03:03', '2017-10-01 23:03:03'),
+(15, 312312, 'ewqe', 'qweqweqweqw', 'wqeqw', 'Female', '312312312', '2017-10-17', 'qwewqew', 0, 'ewqeqwe', 'qweqweqw', b'1111111111111111111111111111111', '2017-10-10 10:48:41', '2017-10-10 10:48:41');
 
 -- --------------------------------------------------------
 
@@ -163,6 +169,30 @@ INSERT INTO `tbl_inv_attachments` (`wat_id`, `file_name`, `file_path`, `full_pat
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `tbl_inv_pcategory`
+--
+
+CREATE TABLE `tbl_inv_pcategory` (
+  `ipcat_id` int(11) NOT NULL,
+  `category` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_inv_pcategory`
+--
+
+INSERT INTO `tbl_inv_pcategory` (`ipcat_id`, `category`) VALUES
+(1, 'Common Electrical Supplies'),
+(2, 'Common Office Supplies'),
+(3, 'Common Office Devices'),
+(4, 'Common Janitorial Supplies'),
+(5, 'Common Office Equipment'),
+(6, 'Common Computer Supplies'),
+(7, 'Consumables');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `tbl_inv_products`
 --
 
@@ -183,7 +213,9 @@ INSERT INTO `tbl_inv_products` (`ipr_id`, `product_code`, `product_name`, `produ
 (1, 123, 'prod', 'desc', 'Active', 12),
 (2, 113, 'wala', 'ako', 'Inactive', 12),
 (3, 980, 'shit', 'life', 'Inactive', 0),
-(4, 991, 'no', 'value', 'Inactive', 0);
+(4, 991, 'no', 'value', 'Inactive', 0),
+(5, 0, '', '', 'Inactive', 0),
+(8, 2147483647, '', '', 'Inactive', 0);
 
 -- --------------------------------------------------------
 
@@ -195,9 +227,9 @@ CREATE TABLE `tbl_inv_stocks` (
   `ist_id` int(11) NOT NULL,
   `ist_product_code` int(11) NOT NULL,
   `product_qty` int(11) NOT NULL,
-  `import_date` timestamp NOT NULL,
+  `import_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `product_qty_left` int(11) NOT NULL,
-  `as_of_date` timestamp NOT NULL
+  `as_of_date` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -207,7 +239,10 @@ CREATE TABLE `tbl_inv_stocks` (
 INSERT INTO `tbl_inv_stocks` (`ist_id`, `ist_product_code`, `product_qty`, `import_date`, `product_qty_left`, `as_of_date`) VALUES
 (8, 980, 1, '0000-00-00 00:00:00', 1, '0000-00-00 00:00:00'),
 (9, 980, 1, '2017-10-08 09:18:49', 1, '2017-10-08 09:18:49'),
-(10, 113, 12, '2017-10-08 09:22:30', 12, '2017-10-08 09:22:30');
+(10, 113, 12, '2017-10-08 09:22:30', 12, '2017-10-08 09:22:30'),
+(11, 980, 0, '2017-10-09 23:41:22', 0, '2017-10-09 23:41:22'),
+(12, 980, 0, '2017-10-09 23:41:26', 0, '2017-10-09 23:41:26'),
+(13, 123, 123, '2017-10-09 23:42:07', 123, '2017-10-09 23:42:07');
 
 -- --------------------------------------------------------
 
@@ -292,10 +327,7 @@ CREATE TABLE `tbl_wbs_attachments` (
 --
 
 INSERT INTO `tbl_wbs_attachments` (`wat_id`, `file_name`, `file_path`, `full_path`) VALUES
-(1, 'asd.txt', 'C:/wamp64/www/School-Mgmt-System/uploads/news/', 'C:/wamp64/www/School-Mgmt-System/uploads/news/asd.txt'),
-(2, 'AljohnPangilinan.docx', 'C:/wamp64/www/School-Mgmt-System/uploads/news/', 'C:/wamp64/www/School-Mgmt-System/uploads/news/AljohnPangilinan.docx'),
-(3, 'HannahLiao.docx', 'uploads/news/', 'C:/wamp64/www/School-Mgmt-System/uploads/news/HannahLiao.docx'),
-(4, 'HannahLiao.docx', 'uploads/news/', 'C:/wamp64/www/School-Mgmt-System/uploads/news/HannahLiao.docx');
+(1, 'asd.txt', 'C:/wamp64/www/School-Mgmt-System/uploads/news/', 'C:/wamp64/www/School-Mgmt-System/uploads/news/asd.txt');
 
 -- --------------------------------------------------------
 
@@ -358,6 +390,12 @@ ALTER TABLE `students_info`
 --
 ALTER TABLE `tbl_inv_attachments`
   ADD PRIMARY KEY (`wat_id`);
+
+--
+-- Indexes for table `tbl_inv_pcategory`
+--
+ALTER TABLE `tbl_inv_pcategory`
+  ADD PRIMARY KEY (`ipcat_id`);
 
 --
 -- Indexes for table `tbl_inv_products`
@@ -426,37 +464,42 @@ ALTER TABLE `tb_lrmds_attachments`
 -- AUTO_INCREMENT for table `addresses`
 --
 ALTER TABLE `addresses`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 --
 -- AUTO_INCREMENT for table `guardians`
 --
 ALTER TABLE `guardians`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 --
 -- AUTO_INCREMENT for table `parents`
 --
 ALTER TABLE `parents`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 --
 -- AUTO_INCREMENT for table `students_info`
 --
 ALTER TABLE `students_info`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 --
 -- AUTO_INCREMENT for table `tbl_inv_attachments`
 --
 ALTER TABLE `tbl_inv_attachments`
   MODIFY `wat_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
+-- AUTO_INCREMENT for table `tbl_inv_pcategory`
+--
+ALTER TABLE `tbl_inv_pcategory`
+  MODIFY `ipcat_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+--
 -- AUTO_INCREMENT for table `tbl_inv_products`
 --
 ALTER TABLE `tbl_inv_products`
-  MODIFY `ipr_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `ipr_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 --
 -- AUTO_INCREMENT for table `tbl_inv_stocks`
 --
 ALTER TABLE `tbl_inv_stocks`
-  MODIFY `ist_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `ist_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 --
 -- AUTO_INCREMENT for table `tbl_inv_users`
 --
@@ -481,26 +524,12 @@ ALTER TABLE `tbl_sms_users`
 -- AUTO_INCREMENT for table `tbl_wbs_attachments`
 --
 ALTER TABLE `tbl_wbs_attachments`
-  MODIFY `wat_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
---
--- AUTO_INCREMENT for table `tbl_wbs_uploads`
---
-ALTER TABLE `tbl_wbs_uploads`
-  MODIFY `wup_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `wat_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT for table `tb_lrmds_attachments`
 --
 ALTER TABLE `tb_lrmds_attachments`
-  MODIFY `lat_id` int(10) NOT NULL AUTO_INCREMENT;
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `tbl_inv_stocks`
---
-ALTER TABLE `tbl_inv_stocks`
-  ADD CONSTRAINT `tbl_inv_stocks_ibfk_1` FOREIGN KEY (`ist_product_code`) REFERENCES `tbl_inv_products` (`product_code`);
+  MODIFY `lat_id` int(10) NOT NULL AUTO_INCREMENT;COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
