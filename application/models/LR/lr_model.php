3577ff663f500ca5699684e19e18d87e7ccd49d7
Aljohn Pangilinan <pangilinan.aljohn@gmail.com>
2018-02-27 05:02:48 +0800

    adding function in controller and model

--- application/models/LR/lr_model.php
+++ application/models/LR/lr_model.php
@@ -5,10 +5,19 @@ class lr_model extends CI_Model{
 
 	}
 
-  public function add_user($m_data){
-    $this->db->insert('tbl_lr_users_profile', $m_data[0]);
-    $this->db->insert('tbl_lr_users_account', $m_data[1]);
-  }
+	public function add_user($m_data){
+	    $this->db->insert('tbl_lr_users_profile', $m_data[0]);
+	    $this->db->insert('tbl_lr_users_account', $m_data[1]);
+	}
+
+	public function uplaod_lrn($m_data){
+		$this->db->insert('tbl_lr_attachment_info', $m_data[0]);
+		$this->db->insert('tbl_lr_attachment', $m_data[1]);
+	}
+
+	public function add_topic($m_data){
+		$this->db->insert('tbl_lr_attachment_info', $m_data);
+	}
 
 
 }
