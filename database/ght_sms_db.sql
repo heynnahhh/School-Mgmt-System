52fa7fd75b2e0585f283e1c317f83b286113fce7
Aljohn Pangilinan <pangilinan.aljohn@gmail.com>
2018-02-27 18:43:16 +0800

    Adding subjects in SHS

--- database/ght_sms_db.sql
+++ database/ght_sms_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Feb 27, 2018 at 08:36 AM
+-- Generation Time: Feb 27, 2018 at 10:42 AM
 -- Server version: 5.7.19
 -- PHP Version: 5.6.31
 
@@ -500,24 +500,24 @@ INSERT INTO `tbl_lr_grade_lvl` (`lro_id`, `educ_type`, `grade_lvl`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tbl_lr_subjects`
+-- Table structure for table `tbl_lr_jhs_subjects`
 --
 
-DROP TABLE IF EXISTS `tbl_lr_subjects`;
-CREATE TABLE IF NOT EXISTS `tbl_lr_subjects` (
+DROP TABLE IF EXISTS `tbl_lr_jhs_subjects`;
+CREATE TABLE IF NOT EXISTS `tbl_lr_jhs_subjects` (
   `lres_id` int(11) NOT NULL AUTO_INCREMENT,
   `educ_type` varchar(40) NOT NULL,
   `subject` varchar(155) NOT NULL,
   PRIMARY KEY (`lres_id`),
   KEY `educ_type` (`educ_type`),
   KEY `subject` (`subject`)
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `tbl_lr_subjects`
+-- Dumping data for table `tbl_lr_jhs_subjects`
 --
 
-INSERT INTO `tbl_lr_subjects` (`lres_id`, `educ_type`, `subject`) VALUES
+INSERT INTO `tbl_lr_jhs_subjects` (`lres_id`, `educ_type`, `subject`) VALUES
 (1, 'Junior High School', 'Araling Panlipunan'),
 (2, 'Junior High School', 'Arts'),
 (3, 'Junior High School', 'Values Education'),
@@ -534,6 +534,66 @@ INSERT INTO `tbl_lr_subjects` (`lres_id`, `educ_type`, `subject`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `tbl_lr_shs_subjects`
+--
+
+DROP TABLE IF EXISTS `tbl_lr_shs_subjects`;
+CREATE TABLE IF NOT EXISTS `tbl_lr_shs_subjects` (
+  `lres_id` int(11) NOT NULL AUTO_INCREMENT,
+  `educ_type` varchar(40) NOT NULL,
+  `subject_type` varchar(155) NOT NULL,
+  `strand` varchar(155) DEFAULT NULL,
+  `subject` varchar(155) NOT NULL,
+  PRIMARY KEY (`lres_id`),
+  KEY `educ_type` (`educ_type`)
+) ENGINE=InnoDB AUTO_INCREMENT=38 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_lr_shs_subjects`
+--
+
+INSERT INTO `tbl_lr_shs_subjects` (`lres_id`, `educ_type`, `subject_type`, `strand`, `subject`) VALUES
+(1, 'Senior High School', 'Specialized Subject', 'STEM', 'General Chemistry 1'),
+(2, 'Senior High School', 'Applied Subject', NULL, 'Entrepreneurship'),
+(3, 'Senior High School', 'Core Subject', NULL, 'Oral Communication'),
+(4, 'Senior High School', 'Core Subject', NULL, 'Personal Development'),
+(5, 'Senior High School', 'Core Subject', NULL, 'Introduction to the Philosophy of the Human Person'),
+(6, 'Senior High School', 'Core Subject', NULL, 'Komunikasyon at Pananaliksik sa Wika at Kulturang Pilipino'),
+(7, 'Senior High School', 'Core Subject', NULL, 'General Mathematics'),
+(8, 'Senior High School', 'Core Subject', NULL, 'Earth and Life Science'),
+(9, 'Senior High School', 'Core Subject', NULL, 'PE and Health 1'),
+(10, 'Senior High School', 'Specialized Subject', 'HUMSS', 'Philippine Politics and Governance'),
+(11, 'Senior High School', 'Specialized Subject', 'HUMSS', 'Disciplines and Ideas in the Social Sciences'),
+(12, 'Senior High School', 'Specialized Subject', 'GAS', 'Elective 1 - General Biology 1'),
+(13, 'Senior High School', 'Specialized Subject', 'HE', 'Food and Beverages Services 1 and 2'),
+(14, 'Senior High School', 'Specialized Subject', 'Automotive', 'Automotive Servicing 1 and 2'),
+(15, 'Senior High School', 'Specialized Subject', 'GAS', 'Organization and Management'),
+(16, 'Senior High School', 'Specialized Subject', 'ABM', 'Organization and Management'),
+(17, 'Senior High School', 'Specialized Subject', 'ABM', 'Fundamentals of Accountancy, Business and Management 1'),
+(18, 'Senior High School', 'Specialized Subject', 'STEM', 'Pre-Calculus'),
+(19, 'Senior High School', 'Core Subject', NULL, '21st Century Literature from the Philippines and the World'),
+(20, 'Senior High School', 'Core Subject', NULL, 'PE and Health 3'),
+(21, 'Senior High School', 'Applied Subject', NULL, 'English for Academic and Professional Purposes'),
+(22, 'Senior High School', 'Applied Subject', NULL, 'Research Project'),
+(23, 'Senior High School', 'Applied Subject', NULL, 'Practical Research 1'),
+(24, 'Senior High School', 'Applied Subject', NULL, 'Empowerment Technologies/ICT'),
+(25, 'Senior High School', 'Specialized Subject', 'GAS', 'Philippine Politics and Governance'),
+(26, 'Senior High School', 'Specialized Subject', 'HUMSS', 'Disciplines and Ideas in the Social Sciences'),
+(27, 'Senior High School', 'Specialized Subject', 'GAS', 'Elective 2 - General Biology 2'),
+(28, 'Senior High School', 'Specialized Subject', 'HUMSS', 'Trends, Networks and Critical Thinking in the 21st Century'),
+(29, 'Senior High School', 'Specialized Subject', 'HUMSS', 'Creative Non-Fiction'),
+(30, 'Senior High School', 'Specialized Subject', 'ABM', 'Business Finance'),
+(31, 'Senior High School', 'Specialized Subject', 'ABM', 'Fundamentals of Accountancy, Business and Management 2'),
+(32, 'Senior High School', 'Specialized Subject', 'HE', 'Cookery 1 and 2'),
+(33, 'Senior High School', 'Specialized Subject', 'Automotive', 'Automotive Servicing 5 and 6'),
+(34, 'Senior High School', 'Specialized Subject', 'STEM', 'Pre-conservation, Documentation and Examination'),
+(35, 'Senior High School', 'Specialized Subject', 'STEM', 'Wood Conservation'),
+(36, 'Senior High School', 'Specialized Subject', 'STEM', 'Conservation of Rare Documents'),
+(37, 'Senior High School', 'Specialized Subject', 'HUMSS', 'Philippine Politics and Governance');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `tbl_lr_topics`
 --
 
@@ -541,15 +601,13 @@ DROP TABLE IF EXISTS `tbl_lr_topics`;
 CREATE TABLE IF NOT EXISTS `tbl_lr_topics` (
   `lto_id` int(11) NOT NULL AUTO_INCREMENT,
   `topic_id` int(11) NOT NULL,
-  `educ_type` varchar(40) NOT NULL,
-  `grade_lvl` varchar(40) NOT NULL,
+  `educ_type` varchar(155) NOT NULL,
+  `subject_type` varchar(155) DEFAULT NULL,
+  `strand` varchar(155) DEFAULT NULL,
   `subject` varchar(155) NOT NULL,
   `topic_name` varchar(255) NOT NULL,
   PRIMARY KEY (`lto_id`),
-  UNIQUE KEY `topic_id` (`topic_id`),
-  KEY `educ_type` (`educ_type`),
-  KEY `grade_lvl` (`grade_lvl`),
-  KEY `subject` (`subject`)
+  UNIQUE KEY `topic_id` (`topic_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -831,18 +889,16 @@ ALTER TABLE `tbl_lr_grade_lvl`
   ADD CONSTRAINT `tbl_lr_grade_lvl_ibfk_1` FOREIGN KEY (`educ_type`) REFERENCES `tbl_lr_educ_type` (`educ_type`);
 
 --
--- Constraints for table `tbl_lr_subjects`
+-- Constraints for table `tbl_lr_jhs_subjects`
 --
-ALTER TABLE `tbl_lr_subjects`
-  ADD CONSTRAINT `tbl_lr_subjects_ibfk_1` FOREIGN KEY (`educ_type`) REFERENCES `tbl_lr_educ_type` (`educ_type`);
+ALTER TABLE `tbl_lr_jhs_subjects`
+  ADD CONSTRAINT `tbl_lr_jhs_subjects_ibfk_1` FOREIGN KEY (`educ_type`) REFERENCES `tbl_lr_educ_type` (`educ_type`);
 
 --
--- Constraints for table `tbl_lr_topics`
+-- Constraints for table `tbl_lr_shs_subjects`
 --
-ALTER TABLE `tbl_lr_topics`
-  ADD CONSTRAINT `tbl_lr_topics_ibfk_1` FOREIGN KEY (`educ_type`) REFERENCES `tbl_lr_educ_type` (`educ_type`),
-  ADD CONSTRAINT `tbl_lr_topics_ibfk_2` FOREIGN KEY (`grade_lvl`) REFERENCES `tbl_lr_grade_lvl` (`grade_lvl`),
-  ADD CONSTRAINT `tbl_lr_topics_ibfk_3` FOREIGN KEY (`subject`) REFERENCES `tbl_lr_subjects` (`subject`);
+ALTER TABLE `tbl_lr_shs_subjects`
+  ADD CONSTRAINT `tbl_lr_shs_subjects_ibfk_1` FOREIGN KEY (`educ_type`) REFERENCES `tbl_lr_educ_type` (`educ_type`);
 
 --
 -- Constraints for table `tbl_lr_users_account`
